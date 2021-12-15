ec6c4df50a78de909583ad54158006c4bc301119
Noah Blumenstein <noah.blumenstein@gmail.com>
2021-12-14 22:11:25 -0500

    commit tree 340d52109847e46b68ff53b962dfeaf1f664232e

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,11 @@
-# your code goes here
+class Person
+    attr_reader :name
+    attr_accessor :bank_account, :happiness
+    def initialize(name, bank_account = 25, happiness = 8)
+        @name=name 
+        @bank_account=bank_account
+        @happiness=happiness
+    end
+
+
+end
